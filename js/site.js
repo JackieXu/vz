53d6a4b5474a16a7c5ffeb828047ca502558a5da
Jackie Xu <jackie@jackiexu.com>
2015-10-26 17:55:21 +0100

    Bug fixes

---
+++ js/site.js
@@ -0,0 +1,51 @@
+(function () {
+    "use strict";
+
+    var emailForms = document.querySelectorAll('.invite-form'),
+        phoneScreens = document.querySelectorAll('.phone-image'),
+        explanationCells = document.querySelectorAll('.explanation-cell'),
+        explanationBars = document.querySelectorAll('.progress-bar'),
+        explanationCellBars = document.querySelectorAll('.progress-bar-filled'),
+        currentScreenIndex = 0;
+
+    [].forEach.call(emailForms, function (form) {
+        form.addEventListener('submit', function (event) {
+            event.stopPropagation();
+            event.preventDefault();
+
+            console.log('Hi');
+
+            return false;
+        }, false);
+    });
+
+    function renderScreens() {
+        var currentCellBar = explanationCellBars[currentScreenIndex],
+            width = parseInt(currentCellBar.style.width, 10) || 0;
+
+        width = (width + 10) % 100;
+
+        if (width === 0) {
+            currentScreenIndex = (currentScreenIndex + 1) % 3;
+
+            [].forEach.call(phoneScreens, function (screen, index) {
+                screen.classList.toggle('visible', index === currentScreenIndex);
+            });
+
+            [].forEach.call(explanationCells, function (cell, index) {
+                cell.classList.toggle('active', index === currentScreenIndex);
+            });
+
+            [].forEach.call(explanationBars, function (bar, index) {
+                bar.classList.toggle('hidden', index !== currentScreenIndex);
+                bar.classList.toggle('active', index === currentScreenIndex);
+            });
+        }
+
+        currentCellBar.style.width = width + '%';
+
+        setTimeout(renderScreens, 1000);
+    }
+
+    renderScreens();
+}());
