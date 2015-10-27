afd40965d5a70c756270d2f045809a34f2a25ae3
Jackie Xu <jackie@miwi.com>
2015-10-27 15:15:15 +0100

    Bug fixes

--- js/site.js
+++ js/site.js
@@ -6,7 +6,8 @@
         explanationCells = document.querySelectorAll('.explanation-cell'),
         explanationBars = document.querySelectorAll('.progress-bar'),
         explanationCellBars = document.querySelectorAll('.progress-bar-filled'),
-        currentScreenIndex = 0;
+        currentScreenIndex = 0,
+        timer;
 
     [].forEach.call(emailForms, function (form) {
         form.addEventListener('submit', function (event) {
@@ -21,13 +22,32 @@
 
     [].forEach.call(explanationCells, function (cell, index) {
         cell.addEventListener('click', function () {
+            currentScreenIndex = index;
+            [].forEach.call(explanationCellBars, function (bar, index) {
+                bar.dataset.width = 0;
+            });
+            [].forEach.call(phoneScreens, function (screen, index) {
+                screen.classList.toggle('visible', index === currentScreenIndex);
+            });
 
+            [].forEach.call(explanationCells, function (cell, index) {
+                cell.classList.toggle('active', index === currentScreenIndex);
+            });
+
+            [].forEach.call(explanationBars, function (bar, index) {
+                bar.classList.toggle('hidden', index !== currentScreenIndex);
+                bar.classList.toggle('active', index === currentScreenIndex);
+            });
+            clearTimeout(timer);
+            timer = renderScreens();
         }, false);
     });
 
     function renderScreens() {
         var currentCellBar = explanationCellBars[currentScreenIndex],
-            width = parseInt(currentCellBar.style.width, 10) || 0;
+            width = parseInt(currentCellBar.dataset.width, 10);
+
+        console.log(currentScreenIndex);
 
         width = (width + 25) % 125;
 
@@ -48,9 +68,12 @@
             });
         }
 
+        currentCellBar.dataset.width = width;
         currentCellBar.style.width = width + '%';
 
-        setTimeout(renderScreens, 1000);
+        timer = setTimeout(renderScreens, 1000);
+
+        return timer;
     }
 
     renderScreens();
