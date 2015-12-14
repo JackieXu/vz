409b0dcc22ad366d56255bc3588d869a21272f8a
Jackie Xu <jackie@miwi.com>
2015-12-14 18:33:43 +0100

    Bug fixes

--- js/site.js
+++ js/site.js
@@ -92,4 +92,17 @@
     }
 
     renderScreens();
+
+    onePageScroll(".main", {
+        sectionContainer: "section",
+        easing: "ease",
+        animationTime: 1000,
+        pagination: true,
+        updateURL: false,
+        beforeMove: function(index) {},
+        afterMove: function(index) {},
+        loop: false,
+        keyboard: true,
+        responsiveFallback: false
+    });
 }());
